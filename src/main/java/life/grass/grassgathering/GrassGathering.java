94cc9446249192b7cb9f30f415a3e45930bd21b2
cyclic-ester <Cyclic_Ester@yahoo.co.jp>
2017-06-18 23:08:17 +0900

    [fix] modify constructor from config.yml

--- src/main/java/life/grass/grassgathering/GrassGathering.java
+++ src/main/java/life/grass/grassgathering/GrassGathering.java
@@ -2,10 +2,14 @@ package life.grass.grassgathering;
 
 import life.grass.grassgathering.fishing.event.PlayerFishingEvent;
 import life.grass.grassgathering.mining.event.MiningEvent;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class GrassGathering extends JavaPlugin {
+import java.io.File;
+import java.io.IOException;
 
+public final class GrassGathering extends JavaPlugin {
 
     public static GrassGathering instance;
 
@@ -16,13 +20,32 @@ public final class GrassGathering extends JavaPlugin {
     @Override
     public void onEnable() {
         super.onEnable();
-        instance  = this;
+        instance = this;
         getServer().getPluginManager().registerEvents(new PlayerFishingEvent(), this);
         getServer().getPluginManager().registerEvents(new MiningEvent(), this);
+        createFile();
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+
+    private void createFile() {
+        try {
+            if (!getDataFolder().exists()) {
+                getDataFolder().mkdirs();
+            }
+            File file = new File(getDataFolder(), "config.yml");
+            if (!file.exists()) {
+                getLogger().info("config.yml not found, creating...");
+                saveDefaultConfig();
+            } else {
+                getLogger().info("config.yml found, loading...");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
